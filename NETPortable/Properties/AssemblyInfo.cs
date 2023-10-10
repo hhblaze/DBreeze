f7cc9d45467b555dc2fb4205ccdab8e4f5727270
#OpKremlin <hhblaze@gmail.com>
2023-10-10 23:34:35 +0200

    Embedding Vectors Layer

--- NETPortable/Properties/AssemblyInfo.cs
+++ NETPortable/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.108.2023.0728")]
-[assembly: AssemblyFileVersion("1.108.2023.0728")]
+[assembly: AssemblyVersion("1.109.2023.1011")]
+[assembly: AssemblyFileVersion("1.109.2023.1011")]
 
