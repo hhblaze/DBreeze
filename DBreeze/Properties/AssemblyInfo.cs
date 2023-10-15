891610b7f10be0e89c2e42704dd777b9576715bd
#OpKremlin <hhblaze@gmail.com>
2023-10-15 12:23:25 +0200

    Optimizing/fixing Vector layer

--- DBreeze/Properties/AssemblyInfo.cs
+++ DBreeze/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.112.2023.1013")]
-[assembly: AssemblyFileVersion("1.112.2023.1013")]
+[assembly: AssemblyVersion("1.113.2023.1015")]
+[assembly: AssemblyFileVersion("1.113.2023.1015")]
--- Deployment/!!!ChangeVersionAndbuild.bat
+++ Deployment/!!!ChangeVersionAndbuild.bat
@@ -1,4 +1,4 @@
-"%~dp0Deployer\Deployer\bin\Debug\Deployer.exe" "1.112.2023.1013"
+"%~dp0Deployer\Deployer\bin\Debug\Deployer.exe" "1.113.2023.1015"
 "%~dp0Nuspec\!!!Build.bat"
 
 echo Done...
