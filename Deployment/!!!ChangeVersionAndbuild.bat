faaf4a5ac6d002fde76e5cbeed7e7ffc66457e61
#OpKremlin <hhblaze@gmail.com>
2023-11-03 23:26:23 +0100

    Release 115

--- Deployment/!!!ChangeVersionAndbuild.bat
+++ Deployment/!!!ChangeVersionAndbuild.bat
@@ -1,4 +1,4 @@
-"%~dp0Deployer\Deployer\bin\Debug\Deployer.exe" "1.114.2023.1016"
+"%~dp0Deployer\Deployer\bin\Debug\Deployer.exe" "1.115.2023.1103"
 "%~dp0Nuspec\!!!Build.bat"
 
 echo Done...
--- NETPortable/Properties/AssemblyInfo.cs
+++ NETPortable/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.114.2023.1016")]
-[assembly: AssemblyFileVersion("1.114.2023.1016")]
+[assembly: AssemblyVersion("1.115.2023.1103")]
+[assembly: AssemblyFileVersion("1.115.2023.1103")]
 
